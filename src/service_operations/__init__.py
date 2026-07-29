87954d4ca10119464a1d7db2b263d3a9649ee525
Tobias Wietelmann <tobias.j.wietelmann@gmail.com>
2026-07-29 11:49:00 +0200

    Export Medallion pipeline API

--- src/service_operations/__init__.py
+++ src/service_operations/__init__.py
@@ -1,14 +1,28 @@
-"""Service-operations data baseline for the Fabric portfolio project."""
+"""Service-operations analytics pipeline for the Microsoft Fabric portfolio project."""
 
 from service_operations.contracts import load_contract
 from service_operations.generator import generate_dataframe, write_dataset
+from service_operations.medallion import (
+    MedallionTables,
+    build_bronze,
+    build_medallion,
+    build_silver,
+    run_medallion,
+    write_medallion,
+)
 from service_operations.validation import ValidationResult, validate_dataframe, validate_file
 
 __all__ = [
+    "MedallionTables",
     "ValidationResult",
+    "build_bronze",
+    "build_medallion",
+    "build_silver",
     "generate_dataframe",
     "load_contract",
+    "run_medallion",
     "validate_dataframe",
     "validate_file",
     "write_dataset",
+    "write_medallion",
 ]
